c97dbd6b3f968efbc2aa6d7b620df132bf6e7476
Michael <m9810223@gmail.com>
2024-04-09 03:15:14 +0800

    feat: release

--- src/pwright/typealiases.py
+++ src/pwright/typealiases.py
@@ -3,8 +3,9 @@ import typing as t
 
 
 T = t.TypeVar('T')
+P = t.TypeVar('P')
 
 SecondsT = t.Union[float, timedelta]
 
-GeneratorContextManager = t.Callable[[t.Any], t.ContextManager[T]]
-AsyncGeneratorContextManager = t.Callable[[t.Any], t.AsyncContextManager[T]]
+GeneratorContextManager = t.Callable[[P], t.ContextManager[T]]
+AsyncGeneratorContextManager = t.Callable[[P], t.AsyncContextManager[T]]
